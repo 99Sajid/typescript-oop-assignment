95d42b4a40ccf0ccc87c9a3c1f899759b106e73e
Md. Shajid Rahman <shajiedrahman@gmail.com>
2026-05-16 10:54:02 +0600

    added 5th problem

--- src/solutions.ts
+++ src/solutions.ts
@@ -34,3 +34,20 @@ function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
 }
 const user = { id: 1, name: "John Doe", age: 21 };
 
+
+//problem-05
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+
+function toggleReadStatus(book: Book): Book & { isRead: boolean } {
+  return {
+    ...book,
+    isRead: true,
+  };
+}
+const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
+console.log(toggleReadStatus(myBook));
+

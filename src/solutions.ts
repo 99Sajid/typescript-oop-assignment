18d31ee039227d142db093bf3b638635f87ea0e5
Md. Shajid Rahman <shajiedrahman@gmail.com>
2026-05-16 10:52:15 +0600

    added 4th problem

--- src/solutions.ts
+++ src/solutions.ts
@@ -27,3 +27,10 @@ function checkType(value: StringOrNumber): string {
 
   return "Number";
 }
+
+//problem-04
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+}
+const user = { id: 1, name: "John Doe", age: 21 };
+
